6a7144d682b312c5efd1e5a4581846a1585793c0
jcul22 <66035684+jcul22@users.noreply.github.com>
2022-02-10 11:29:25 -0600

    Added Flick Part 2

---
+++ Flick/MovieGridCell.swift
@@ -0,0 +1,16 @@
+//
+//  MovieGridCell.swift
+//  Flick
+//
+//  Created by user210689 on 2/10/22.
+//
+
+import UIKit
+
+class MovieGridCell: UICollectionViewCell {
+    
+    @IBOutlet weak var posterView: UIImageView!
+    
+    
+    
+}
---
+++ Flick/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Flick
+//
+//  Created by user210689 on 1/25/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
